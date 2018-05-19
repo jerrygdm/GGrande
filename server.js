7d6b72ecc592b8e4e82d4a71e189dd5065274ec0
Gianmaria Dal Maistro <gianmaria.dalmaistro@me.com>
2018-05-19 18:22:22 +0200

    serve

--- server.js
+++ server.js
@@ -9,13 +9,9 @@ var USERS_COLLECTION = "users";
 var app = express();
 app.use(bodyParser.json());
 
-var distDir = __dirname + "/dist/";
-app.use(express.static(distDir));
-app.get('/*', function(req,res) {
-    
-  res.sendFile(path.join(__dirname + '/index.html'));
-  });
-  
-  // Start the app by listening on the default Heroku port
-  app.listen(process.env.PORT || 8080);
-
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
