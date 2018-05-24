bce1fef1d2493e23ee15af109876803126752244
Gianmaria Dal Maistro <gianmaria.dalmaistro@me.com>
2018-05-24 22:43:19 +0200

    dist

--- server.js
+++ server.js
@@ -9,9 +9,10 @@ var USERS_COLLECTION = "users";
 var app = express();
 app.use(bodyParser.json());
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+app.use(express.static(__dirname + '/dist'));
+
+app.listen(process.env.PORT || 8080);
+
 
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
@@ -23,19 +24,13 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:2701
     process.exit(1);
   }
 
+});
+
   // Save database object from the callback for reuse.
   db = client.db();
   console.log("Database connection ready");
 
-  // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
-});
-
-// CONTACTS API ROUTES BELOW
-
+  
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
   console.log("ERROR: " + reason);
